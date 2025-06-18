b4900d5f123f63bb456384c0799882238556afb4
Hagen <14922841+nathan-diodan@users.noreply.github.com>
2025-06-18 19:06:28 +0000

    aflow: add 4.0.5 bottle.

--- Formula/aflow.rb
+++ Formula/aflow.rb
@@ -5,6 +5,13 @@ class Aflow < Formula
   sha256 "4b250edf96f5318fc8588321a7778e5e271616d6cad3b4cc405a6cacb4c95b19"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/aflow-org/aflow"
+    sha256 cellar: :any,                 arm64_sequoia: "e7e27316dea3c62468613f36174b6fcda09fe3d838a18a559116b045da7f38eb"
+    sha256 cellar: :any,                 ventura:       "bcd4ae56dfd19c31b8d467be7b29cb0480fb9cd3622b6e94617906b1425021ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "920ef0e8f5ff6a71fd31b1bfa11a0aa87ea123e13291c98635af3d5bc96f9967"
+  end
+
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "curl"
