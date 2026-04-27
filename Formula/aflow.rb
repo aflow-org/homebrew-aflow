ea9b7acfb78889c1095d0f2c723a6d1e87df4544
Hagen Eckert <hagen.eckert@nadaro.net>
2026-04-27 11:42:02 -0700

    update to 4.1

--- Formula/aflow.rb
+++ Formula/aflow.rb
@@ -1,16 +1,9 @@
 class Aflow < Formula
   desc "Automatic FLOW for Materials Discovery"
   homepage "https://aflow.org"
-  url "https://github.com/aflow-org/aflow/releases/download/v4.0.5/aflow_4.0.5.orig.tar.gz"
-  sha256 "4b250edf96f5318fc8588321a7778e5e271616d6cad3b4cc405a6cacb4c95b19"
+  url "https://github.com/aflow-org/aflow/releases/download/v4.1/aflow_4.1.orig.tar.gz"
+  sha256 "3711ed521e97c8a005d89720a4d990930ab3d928f5d2bf718a713c0d0e0f3d0a"
   license "GPL-3.0-only"
-
-  bottle do
-    root_url "https://ghcr.io/v2/aflow-org/aflow"
-    sha256 cellar: :any,                 arm64_sequoia: "e7e27316dea3c62468613f36174b6fcda09fe3d838a18a559116b045da7f38eb"
-    sha256 cellar: :any,                 ventura:       "bcd4ae56dfd19c31b8d467be7b29cb0480fb9cd3622b6e94617906b1425021ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "920ef0e8f5ff6a71fd31b1bfa11a0aa87ea123e13291c98635af3d5bc96f9967"
-  end
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
